d14c7eaff334ff638063ca9eb2a8bf0f088e54ba
HP <HP@Swathi>
2026-01-16 16:06:58 +0530

    Opporunitis, added new pom claas

---
+++ ComcatCRMGUIFramework/src/main/java/com/cast/crm/objectrepositortutility/Opportunities.java
@@ -0,0 +1,11 @@
+package com.cast.crm.objectrepositortutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Opportunities {
+	
+	@FindBy(xpath = "//input[@alt='Create Opportunity...']")
+private WebElement createOpportunitImgBtn;	
+
+}
